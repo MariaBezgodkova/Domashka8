3b56ff3af5d989fcec280545e9b2b46665a9f0e8
MariaBezgodkova <ask.2004@yandex.ru>
2023-03-09 10:34:12 +0300

    Упорядочивание массива по строкам от мин к макс

---
+++ DZ54/Program.cs
@@ -0,0 +1,48 @@
+﻿ //Задайте двумерный массив. Напишите программу, которая упорядочит
+  // по убыванию элементы каждой строки двумерного массива.
+
+  void Printmatr(int[,] matr)  // метод для печати двумерного массива
+{
+    for (int i = 0; i < matr.GetLength(0); i++)  // GetLength(0) - чтобы не писать каждый раз кол-во строк
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)  // GetLength(1) - чтобы не писать каждый раз кол-во столбцов
+        {
+            Console.Write($"{matr[i, j]} " + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+void Fillmatr(int[,] matr)// метод для заполнения матрицы случайными числами
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void Order(int[,] matr)// метод для заполнения матрицы случайными числами
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(1)-1; k++)
+            {
+                if (matr[i, k] < matr[i, k + 1])
+                {
+                    int temp = matr[i, k + 1];
+                    matr[i, k + 1] = matr[i, k];
+                    matr[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+int[,] matrix = new int[3, 4];
+Fillmatr(matrix);
+Printmatr(matrix);
+Order(matrix);
+Console.WriteLine("----------------");
+Printmatr(matrix);
